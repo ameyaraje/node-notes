4d353670bb4419224e40e09db0e16ecbe038bb0c
Ameya Raje <ameyacraje@gmail.com>
2020-08-28 08:09:38 -0400

    S4-16: added yargs and some basic user input stuff

--- app.js
+++ app.js
@@ -1,3 +1,37 @@
-const getNotes = require('./notes');
+const validator = require('validator');
+const chalk = require('chalk');
+const yargs = require('yargs');
 
-getNotes('hmmm');
+yargs.command({
+    command: 'add',
+    decribe: 'Add a new note',
+    handler: function () {
+        console.log(chalk.green('Adding a new note'));
+    }
+});
+
+yargs.command({
+    command: 'remove',
+    decribe: 'Remove an existing note',
+    handler: function () {
+        console.log(chalk.red('Removing selected note'));
+    }
+});
+
+yargs.command({
+    command: 'list',
+    decribe: 'List all notes',
+    handler: function () {
+        console.log(chalk.green('Listing all notes'));
+    }
+});
+
+yargs.command({
+    command: 'view',
+    decribe: 'View selected note',
+    handler: function () {
+        console.log(chalk.blue('Viewing selected note'));
+    }
+});
+
+console.log(yargs.argv);
